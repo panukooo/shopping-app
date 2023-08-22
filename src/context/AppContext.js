1eb324c349c54b7b3c81733f38cd98ccd5d9dc6f
panukooo <vinyard.derek62@gmail.com>
2023-08-21 22:48:23 -0400

    nada

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,7 +2,6 @@ import React, { createContext, useReducer } from 'react';
 
 let initialState = {
     cartValue = 0,
-    quantity = 0,
     locations: [
         {location: "Russia", simbol: "₽"},
         {location: "India", simbol: "₹"},
@@ -12,11 +11,11 @@ let initialState = {
         {location: "China", simbol: "¥"}
     ],
     items: [
-        {itemName: "Shirt", price: 500, quantity: 0},
-        {itemName: "Jeans", price: 300, quantity: 0},
-        {itemName: "T-Shirt", price: 150, quantity: 0},
-        {itemName: "Watch", price: 950, quantity: 0},
-        {itemName: "Dress", price: 820, quantity: 0},
-        {itemName: "Shoes", price: 746, quantity: 0}
+        {itemName: "Shirt", price: 500, quantity: 0, total: 0},
+        {itemName: "Jeans", price: 300, quantity: 0, total:0},
+        {itemName: "T-Shirt", price: 150, quantity: 0, total: 0},
+        {itemName: "Watch", price: 950, quantity: 0, total:0},
+        {itemName: "Dress", price: 820, quantity: 0, total:0},
+        {itemName: "Shoes", price: 746, quantity: 0, total:0}
     ]
 };
