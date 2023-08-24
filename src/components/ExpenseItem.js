d3ef6b8b8ed9136013d23a6367e204f35563ff6b
panukooo <vinyard.derek62@gmail.com>
2023-08-23 23:32:59 -0400

    yoou

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,7 +6,7 @@ const ExpenseItem = (props) => {
     const {dispatch, globalLocation} = useContext(AppContext);
     const roundTwo = Math.pow(10, 2);
     const unitPrice = Math.round(props.price * props.exch * roundTwo) / roundTwo;
-    const finalPrice = Math.round(unitPrice * props.quantity * roundTwo) / roundTwo;
+    const totalPrice = Math.round(unitPrice * props.quantity * roundTwo) / roundTwo;
 
     const deleteQuantity = () => {
         dispatch({
@@ -20,7 +20,7 @@ const ExpenseItem = (props) => {
             <td>{props.name}</td>
             <td>{props.quantity}</td>
             <td>{globalLocation}{unitPrice}</td>
-            <td>{globalLocation}{finalPrice}</td>
+            <td>{globalLocation}{totalPrice}</td>
             <td><FaTimesCircle size="1.5em" color="red" onClick={deleteQuantity}/></td>
         </tr>
     )
